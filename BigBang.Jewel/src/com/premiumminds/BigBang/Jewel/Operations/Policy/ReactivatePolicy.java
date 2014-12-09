bd42cf76ce5e066d43af8c640ebe945653967d78
JMendes <joao.mendes@premium-minds.com>
2014-12-09 10:49:51 +0000

    Bug na reactivação de apólices quando a anulação não seguiu o circuito tradicional.

--- BigBang.Jewel/src/com/premiumminds/BigBang/Jewel/Operations/Policy/ReactivatePolicy.java
+++ BigBang.Jewel/src/com/premiumminds/BigBang/Jewel/Operations/Policy/ReactivatePolicy.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import Jewel.Engine.Engine;
 import Jewel.Engine.DataAccess.SQLServer;
+import Jewel.Petri.Interfaces.ILog;
 import Jewel.Petri.Objects.PNProcess;
 import Jewel.Petri.SysObjects.JewelPetriException;
 import Jewel.Petri.SysObjects.UndoableOperation;
@@ -50,17 +51,35 @@ public class ReactivatePolicy
 	{
 		Policy lobjPolicy;
 		NotPayedIndication lopNPI;
+		ILog lrefAutoVoid;
+		ILog lrefNPI;
 
 		try
 		{
 			lobjPolicy = (Policy)GetProcess().GetData();
 			mdtEffectDate = (Timestamp)lobjPolicy.getAt(9);
 
-			lopNPI = (NotPayedIndication)PNProcess.GetInstance(Engine.getCurrentNameSpace(),
-					GetProcess().GetLiveLog(Constants.OPID_Policy_ExternAutoVoid, pdb).GetOperationData().GetExternalProcess())
-					.GetLiveLog(Constants.OPID_Receipt_NotPayedIndication, pdb).GetOperationData();
-			lobjPolicy.setAt(13, lopNPI.midPrevStatus);
-			lobjPolicy.setAt(9, lopNPI.mdtPrevEndDate);
+			lopNPI = null;
+			lrefAutoVoid = GetProcess().GetLiveLog(Constants.OPID_Policy_ExternAutoVoid, pdb);
+			if (lrefAutoVoid != null)
+			{
+				lrefNPI = PNProcess.GetInstance(Engine.getCurrentNameSpace(), lrefAutoVoid.GetOperationData().GetExternalProcess())
+						.GetLiveLog(Constants.OPID_Receipt_NotPayedIndication, pdb);
+				if (lrefNPI != null)
+				{
+					lopNPI = (NotPayedIndication)lrefNPI.GetOperationData();
+					lobjPolicy.setAt(13, lopNPI.midPrevStatus);
+					lobjPolicy.setAt(9, lopNPI.mdtPrevEndDate);
+					lobjPolicy.SaveToDb(pdb);
+				}
+			}
+
+			if (lopNPI == null)
+			{
+				lobjPolicy.setAt(13, Constants.StatusID_Valid);
+				lobjPolicy.setAt(9, null);
+			}
+
 			lobjPolicy.SaveToDb(pdb);
 		}
 		catch (Throwable e)
