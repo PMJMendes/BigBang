2cb8bd6c137d6f01f2b69e4c14424888d0c0e0bd
jcamilo-egs <joaocamilo@credite-egs.pt>
2016-04-06 14:39:46 +0100

    Small fix, for I did not merged correctly this file upon release, meaning that the premiums were not being updated on receipt creation.

--- BigBang.Jewel/src/com/premiumminds/BigBang/Jewel/Operations/Policy/CreateReceipt.java
+++ BigBang.Jewel/src/com/premiumminds/BigBang/Jewel/Operations/Policy/CreateReceipt.java
@@ -1,18 +1,23 @@
 package com.premiumminds.BigBang.Jewel.Operations.Policy;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+import Jewel.Engine.DataAccess.SQLServer;
 import Jewel.Petri.SysObjects.JewelPetriException;
 
 import com.premiumminds.BigBang.Jewel.BigBangJewelException;
 import com.premiumminds.BigBang.Jewel.Constants;
+import com.premiumminds.BigBang.Jewel.Objects.Policy;
 import com.premiumminds.BigBang.Jewel.Operations.Receipt.CreateReceiptBase;
 
 public class CreateReceipt
 	extends CreateReceiptBase
 {
 	private static final long serialVersionUID = 1L;
+
+	public boolean premiumChanged;
 
 	public CreateReceipt(UUID pidProcess)
 	{
@@ -64,5 +69,43 @@ public class CreateReceipt
 		{
 			throw new BigBangJewelException(e.getMessage(), e);
 		}
+	}
+	
+	/**
+	 * This method calls the CreateReceiptBase's method "Run", and afterwards checks
+	 * if the created receipt is a continuing receipt, in which case it updates the policy's
+	 * total and sales's premiums.
+	 */
+	protected void Run(SQLServer pdb) throws JewelPetriException {
+		super.Run(pdb);
+
+		premiumChanged = false;
+
+		if ( mobjData.midType.equals(Constants.RecType_Continuing) ) {
+			Policy policy = (Policy)GetProcess().GetData();
+			BigDecimal newPremium = policy.CheckSalesPremium(mobjData.mdblCommercial);
+			BigDecimal newTotalPr = policy.CheckTotalPremium(mobjData.mdblTotal);
+
+			if ((newPremium != null) || (newTotalPr != null)) {
+				try {
+					policy.setAt(Policy.I.PREMIUM, newPremium);
+					policy.setAt(Policy.I.TOTALPREMIUM, newTotalPr);
+					policy.SaveToDb(pdb);
+				} catch (Throwable e) {
+					throw new JewelPetriException(e.getMessage(), e);
+				}
+				premiumChanged = true;
+			}
+		}
+	}
+
+	public String LongDesc(String pstrLineBreak) {
+		String str = super.LongDesc(pstrLineBreak);
+
+		if (premiumChanged) {
+			str = str + "Foram actualizados os prémios total e comercial da apólice." + pstrLineBreak;
+		}
+
+		return str;
 	}
 }
