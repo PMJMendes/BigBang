98adca8ae03ad70155a91e624810f7745c46a654
jcamilo-egs <joaocamilo@credite-egs.pt>
2017-02-03 17:59:59 +0000

    Initial commit

---
+++ BigBang.Jewel/src/com/premiumminds/BigBang/Jewel/SysObjects/PHCConnector.java
@@ -0,0 +1,9 @@
+package com.premiumminds.BigBang.Jewel.SysObjects;
+
+/**
+ *	Class responsible for implementing the needed functionalities relating 
+ *	with creating and storing a file in the PHC
+ */
+public class PHCConnector {
+
+}
---
+++ BigBang.Jewel/src/com/premiumminds/BigBang/Jewel/SysObjects/XMLCreator.java
@@ -0,0 +1,33 @@
+package com.premiumminds.BigBang.Jewel.SysObjects;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import com.premiumminds.BigBang.Jewel.BigBangJewelException;
+
+/**
+ * Class responsible for creating a XML file with the needed info for billing
+ */
+public class XMLCreator {
+
+	private static DocumentBuilderFactory docFactory;
+	private static DocumentBuilder docBuilder;
+
+	private static void initialize() throws BigBangJewelException {
+
+		if (docFactory == null) {
+			docFactory = DocumentBuilderFactory.newInstance();
+		}
+
+		if (docBuilder == null) {
+			try {
+				docBuilder = docFactory.newDocumentBuilder();
+			} catch (Throwable e) {
+				throw new BigBangJewelException(
+						"Error while initializing the DocumentBuilder used for XML creation. "
+								+ e.getMessage(), e);
+			}
+		}
+	}
+
+}
