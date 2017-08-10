2241b2d18c8cca4c9bef659c505259fc67c1b878
jcamilo-egs <joaocamilo@credite-egs.pt>
2017-08-10 16:52:44 +0100

    SQL for the new parameter

---
+++ BigBang.Jewel/SQL/SubCasualtyProductivityMap_Changes.sql
@@ -0,0 +1,8 @@
+
+/*-- -Update(s): [bigbang].[tblReportParams] */
+UPDATE [bigbang].[tblReportParams] SET [_TSUpdate]='2017-08-09T16:19:44.960',[NOrder]=3 WHERE [PK]='0a9a6c63-2dd7-4f93-9aca-a7a600c6f2ba'
+UPDATE [bigbang].[tblReportParams] SET [_TSUpdate]='2017-08-09T16:19:39.490',[NOrder]=4 WHERE [PK]='e59c35e2-4781-4fb0-8183-a7a600c703fc'
+UPDATE [bigbang].[tblReportParams] SET [_TSUpdate]='2017-08-09T16:19:33.887',[NOrder]=5 WHERE [PK]='236e1185-f1df-41d8-bdec-a7a600c71383'
+
+/*-- -Insert(s): [bigbang].[tblReportParams] */
+INSERT INTO [bigbang].[tblReportParams] ([PK],[_TSCreate],[_TSUpdate],[FKType],[Label],[NOrder],[FKOwner],[FKParamType],[Units],[FKReferenceTo],[DefaultValue]) VALUES('7493eaef-a582-4aa6-9cc3-a7ca010d3687','2017-08-09T16:20:10.490','2017-08-09T16:20:10.490','4d801865-dc6b-4a8f-8301-a032012782bf',N'Grupo de Clientes',2,'61a0f89b-980b-45f9-b418-a7a600c41f7c','9f626929-a5f7-4caf-aab3-9f96014a8a9f',null,'dc89d454-cccc-441e-bce3-9ee600afcccd',null);
